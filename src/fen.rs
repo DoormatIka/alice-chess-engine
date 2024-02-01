4cab498cf4fd979163356b3cdc88670dbe0c2abe
Kurenairisu <minec323@gmail.com>
2024-02-01 23:18:09 +0800

    Converted little endian to big endian

--- src/fen.rs
+++ src/fen.rs
@@ -1,33 +1,30 @@
-
 use chess::ChessMove;
 use colored::*;
 
 pub fn print_board_from_fen(fen: &str, targets: &Vec<ChessMove>, moves: &Vec<ChessMove>) {
     let parts: Vec<&str> = fen.split(' ').collect();
-    let mut rows: Vec<&str> = parts[0].split('/').collect();
+    let rows: Vec<&str> = parts[0].split('/').collect();
 
-    rows.reverse();
-
-    for (i, row) in rows.iter().enumerate() { // row number
-        for c in row.chars() {
+    for (i, row) in rows.iter().enumerate() {
+        let mut line = String::new();
+        for (j, c) in row.chars().enumerate() {
             match c {
                 '1'..='8' => {
                     let num = c.to_digit(10).unwrap();
-                    for j in 0..num { // column
-                        let s = if let Some(mov) = moves.iter().find(|v| v.get_dest().to_index() == (8 * i) + j as usize) {
-                            "[ ]".green()
+                    for k in 0..num {
+                        if moves.iter().any(|v| v.get_dest().to_index() == (8 * (7 - i)) + (7 - (j + k as usize)) as usize) {
+                            line.push_str(&"[ ]".green().to_string());
                         } else {
-                            "[ ]".into()
-                        };
-                        print!("{}{}", s, (8 * i) + j as usize);
+                            line.push_str("[ ]");
+                        }
                     }
                 }
-                _ => print!("{}", ColoredString::from(format!("[{}]", c))),
+                _ => line.push_str(&format!("[{}]", c)),
             }
         }
-        println!(" {}", 8-i);
+        println!("{} {}", 8-i, line);
     }
-    println!(" a  b  c  d  e  f  g  h");
+    println!("   a  b  c  d  e  f  g  h");
 
     let turn = match parts[1] {
         "w" => "White's turn",
