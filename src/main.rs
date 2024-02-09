9c68fb0492fb2a0dc0f7324c480b2a0d8846a049
DoormatIka <trashcanartistlily@gmail.com>
2024-02-09 15:09:53 +0800

    eval & search

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{str::FromStr, vec, io::stdin};
 
 use fen::print_board_from_fen;
 use peak_alloc::PeakAlloc;
-use chess::{MoveGen, EMPTY, Game, ChessMove, BoardStatus, Color, Board, Piece};
+use chess::{MoveGen, EMPTY, Game, ChessMove, BoardStatus, Color, Board, Piece, BitBoard};
 use rand::seq::SliceRandom;
 
 pub mod fen;
@@ -36,21 +36,7 @@ fn main() {
             _ => ()
         }
 
-        let mut legal_iterable = MoveGen::new_legal(&board);
-        let targets = board.color_combined(!game.side_to_move());
-
-        let mut capture_moves: Vec<_> = vec![];
-        let mut non_capture_moves: Vec<_> = vec![];
-
-        legal_iterable.set_iterator_mask(*targets);
-        for mov in &mut legal_iterable {
-            capture_moves.push(mov);
-        }
-
-        legal_iterable.set_iterator_mask(!EMPTY);
-        for mov in &mut legal_iterable {
-            non_capture_moves.push(mov);
-        }
+        let (mut capture_moves, mut non_capture_moves) = generate_moves(&board);
 
         print_board_from_fen(game.current_position().to_string().as_str(), &capture_moves, &non_capture_moves);
 
@@ -93,29 +79,154 @@ struct BasicBot {
     board: Board,
 }
 impl BasicBot {
-    fn count_material(&self) {
-        let material = 0;
+    pub fn count_material(&self) -> i32 {
+        let white = get_colored_pieces(&self.board, Color::White);
+        let black = get_colored_pieces(&self.board, Color::Black);
 
-        // currently returns both black and white's pieces.
-        let kings = self.board.pieces(Piece::King).popcnt();
-        let pawns = self.board.pieces(Piece::Pawn).popcnt();
-        let rooks = self.board.pieces(Piece::Rook).popcnt();
-        let queens = self.board.pieces(Piece::Queen).popcnt();
-        let knights = self.board.pieces(Piece::Knight).popcnt();
-        let bishops = self.board.pieces(Piece::Bishop).popcnt();
+        let material_white = self.calculate_material(white);
+        let material_black = self.calculate_material(black);
+
+        let eval = material_white - material_black;
+        let perspective = if self.board.side_to_move() == Color::White {
+            1
+        } else {
+            -1
+        };
+
+        return eval as i32 * perspective;
+    }
+
+    fn calculate_material(&self, pieces: PiecesColored) -> u32 {
+        let mut material = 0;
+
+        material += pieces.pawns.popcnt() * 100;
+        material += pieces.knights.popcnt() * 400;
+        material += pieces.bishops.popcnt() * 300;
+        material += pieces.rooks.popcnt() * 500;
+        material += pieces.queens.popcnt() * 900;
+
+        material
     }
 }
 impl Search for BasicBot {
-    
+    fn search(&self, depth: u8) -> i32 {
+        let negative_infinity = -1000000;
+        let mut board = self.board.clone();
+
+        if depth == 0 {
+            return self.evaluation();
+        }
+        
+        // generate moves here
+        let (mut capture_moves, mut non_capture_moves) = generate_moves(&self.board);
+
+        let mut all_move: Vec<ChessMove> = vec![];
+        all_move.append(&mut capture_moves);
+        all_move.append(&mut non_capture_moves);
+
+        if all_move.len() == 0 {
+            if self.board.checkers().popcnt() != 0 {
+                return negative_infinity;
+            }
+            return 0;
+        }
+
+        let best_eval = negative_infinity;
+
+        for board_move in all_move {
+            // returns the board after the move has been done
+            let new_board = board.make_move_new(board_move);
+            self.search(depth - 1);
+            // undoing the make_move
+            // does this even make sense?
+            board = new_board; 
+        }
+
+        best_eval
+    }
 }
 impl Evaluation for BasicBot {
-
+    fn evaluation(&self) -> i32 {
+        self.count_material()
+    }
 }
 trait Search {
-    fn search() {}
+    fn search(&self, depth: u8) -> i32;
 }
 trait Evaluation {
-    fn evaluation() {}
+    fn evaluation(&self) -> i32;
+}
+
+struct PiecesColored {
+    color: Color,
+    kings: BitBoard,
+    pawns: BitBoard,
+    rooks: BitBoard,
+    queens: BitBoard,
+    knights: BitBoard,
+    bishops: BitBoard,
+}
+
+/**
+ * A tuple is a bad idea isn't it?
+ *
+ * Returns (capture_moves, non_capture_moves)
+ */
+fn generate_moves(board: &Board) -> (Vec<ChessMove>, Vec<ChessMove>) {
+    let mut legal_iterable = MoveGen::new_legal(&board);
+    let targets = board.color_combined(!board.side_to_move());
+
+    let mut capture_moves: Vec<_> = vec![];
+    let mut non_capture_moves: Vec<_> = vec![];
+
+    legal_iterable.set_iterator_mask(*targets);
+    for mov in &mut legal_iterable {
+        capture_moves.push(mov);
+    }
+
+    legal_iterable.set_iterator_mask(!EMPTY);
+    for mov in &mut legal_iterable {
+        non_capture_moves.push(mov);
+    }
+
+    return (capture_moves, non_capture_moves);
+}
+
+/**
+ * Returns the pieces but colored.
+ */
+fn get_colored_pieces(board: &Board, color: Color) -> PiecesColored {
+    // returns every black piece in the board..
+    // but they're all treated the same....
+    //
+    // the nuance that a piece is a bishop or a rook is gone
+    let black = board.color_combined(color).0;
+
+    // currently returns both black and white's pieces.
+    let kings = board.pieces(Piece::King).0;
+    let pawns = board.pieces(Piece::Pawn).0;
+    let rooks = board.pieces(Piece::Rook).0;
+    let queens = board.pieces(Piece::Queen).0;
+    let knights = board.pieces(Piece::Knight).0;
+    let bishops = board.pieces(Piece::Bishop).0;
+
+    // do a "mask" to select the kings from the black pieces
+    let color_kings = BitBoard::new(black & kings); // 001 & 101 = 001
+    let color_pawns = BitBoard::new(black & pawns);
+    let color_rooks = BitBoard::new(black & rooks);
+    let color_queens = BitBoard::new(black & queens);
+    let color_knights = BitBoard::new(black & knights);
+    let color_bishops = BitBoard::new(black & bishops);
+
+    PiecesColored { 
+        color, 
+        kings: color_kings, 
+        pawns: color_pawns, 
+        rooks: color_rooks, 
+        queens: color_queens, 
+        knights: color_knights, 
+        bishops: color_bishops, 
+    }
 }
 
 /**
