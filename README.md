57da39ca7ac3087d033aa577c961321bf4541731
DoormatIka <trashcanartistlily@gmail.com>
2024-02-10 20:43:34 +0800

    read

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Two programmers' second attempt at writing a chess engine.
 
 ### First Attempt
   - [Cirno Chess Engine](<https://github.com/DoormatIka/cirno-chess-engine>) - FP is hell to work with in chess engines. This attempt will use OOP.
+
+### Stuff Implemented
+  - BasicBot (Cirno)
+    - Alpha-beta Pruning
+    - Material Counting
+    - Fail-High
+
+### Todo
+  - Iterative Deepening
+  - Piece-square tables
+  - UCI
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let starting = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let almost_mate = "6k1/1p3ppp/8/8/8/3q1bP1/5K1P/8 b - - 0 1    ";
 
-    let mut game = Game::from_str(almost_mate).unwrap();
+    let mut game = Game::from_str(starting).unwrap();
     let player_color: Option<Color> = Some(Color::Black);
 
     loop {
@@ -59,13 +59,13 @@ fn main() {
                 };
             } else {
                 let mut bot = BasicBot::new(&board);
-                let (eval, chess_move) = bot.search(3);
+                let (eval, chess_move) = bot.search(6);
                 game.make_move(chess_move);
                 println!("Made move with eval {}, {}", eval, chess_move);
             }
         } else {
             let mut bot = BasicBot::new(&board);
-            let (eval, chess_move) = bot.search(3);
+            let (eval, chess_move) = bot.search(6);
             game.make_move(chess_move);
             println!("Made move with eval {}, {}", eval, chess_move);
         }
@@ -77,8 +77,6 @@ fn main() {
 
 struct BasicBot {
     board: Board,
-    best_move: Option<ChessMove>,
-    best_eval: i32,
 }
 trait Search {
     fn search(&mut self, depth: u16) -> (i32, ChessMove);
@@ -110,8 +108,6 @@ impl BasicBot {
     fn new(board: &Board) -> Self {
         Self {
             board: board.clone(),
-            best_move: None,
-            best_eval: -9999999,
         }
     }
 
@@ -152,11 +148,11 @@ impl BasicBot {
         mut beta: i32,
         is_maximizing_player: bool,
     ) -> (i32, Option<ChessMove>) {
-        println!("Depth: {}, Alpha: {}, Beta: {}, Is Maximizing Player: {}", depth, alpha, beta, is_maximizing_player);
+        // println!("Depth: {}, Alpha: {}, Beta: {}, Is Maximizing Player: {}", depth, alpha, beta, is_maximizing_player);
     
         if depth == 0 {
             let evaluation = self.evaluation(board);
-            println!("Leaf node, evaluation: {}", evaluation);
+            // println!("Leaf node, evaluation: {}", evaluation);
             return (evaluation, None);
         }
     
@@ -165,14 +161,14 @@ impl BasicBot {
         all_moves.append(&mut capture_moves);
         all_moves.append(&mut non_capture_moves);
     
-        println!("Generated moves: {:?}", all_moves);
+        // println!("Generated moves: {:?}", all_moves);
     
         if all_moves.len() == 0 {
             if board.checkers().popcnt() != 0 {
-                println!("No moves and in check, returning -1000000");
+                // println!("No moves and in check, Checkmate, returning -1000000");
                 return (-1000000, None);
             }
-            println!("No moves, returning 0");
+            // println!("No moves, Stalemate, returning 0");
             return (0, None);
         }
     
@@ -180,42 +176,59 @@ impl BasicBot {
     
         if is_maximizing_player {
             let mut best_val = -1000000;
-            println!("Maximizing player, initial best value: {}", best_val);
+            // println!("Maximizing player, initial best value: {}", best_val);
     
             for board_move in all_moves.iter() {
                 let board = board.make_move_new(*board_move);
-                let (value, _) = self.internal_search(&board, depth - 1, alpha, beta, !is_maximizing_player);
-                if value > best_val {
-                    best_val = value;
+                let (eval, _) = self.internal_search(&board, depth - 1, alpha, beta, !is_maximizing_player);
+
+                if eval > beta {
+                    // assuming the opponent would never let the player reach this position
+                    //      i.e: "failing high"
+                    return (beta, best_move);
+                }
+
+                if eval > best_val {
+                    best_val = eval;
                     best_move = Some(*board_move);
                 }
                 alpha = cmp::max(alpha, best_val);
     
-                println!("Move: {:?}, Value: {}, Best Value: {}, Alpha: {}", board_move, value, best_val, alpha);
+                // println!("Move: {:?}, Value: {}, Best Value: {}, Alpha: {}", board_move, value, best_val, alpha);
     
                 if beta <= alpha {
-                    println!("Alpha >= Beta, pruning");
+                    // println!("Alpha >= Beta, pruning");
                     break;
                 }
             }
             (best_val, best_move)
         } else {
             let mut best_val = 1000000;
-            println!("Minimizing player, initial best value: {}", best_val);
+            // println!("Minimizing player, initial best value: {}", best_val);
     
             for board_move in all_moves.iter() {
                 let board = board.make_move_new(*board_move);
-                let (value, _) = self.internal_search(&board, depth - 1, alpha, beta, !is_maximizing_player);
-                if value < best_val {
-                    best_val = value;
+                let (eval, _) = self.internal_search(&board, depth - 1, alpha, beta, !is_maximizing_player);
+
+
+                if eval < alpha {
+                    // assuming the opponent would never let the player reach this position
+                    //      i.e: "failing high"
+                    //
+                    // same as the maximizing player but just in reverse.
+                    return (alpha, best_move);
+                }
+
+                if eval < best_val {
+                    best_val = eval;
                     best_move = Some(*board_move);
                 }
                 beta = cmp::min(beta, best_val);
     
-                println!("Move: {:?}, Value: {}, Best Value: {}, Beta: {}", board_move, value, best_val, beta);
+                // println!("Move: {:?}, Value: {}, Best Value: {}, Beta: {}", board_move, value, best_val, beta);
     
                 if beta <= alpha {
-                    println!("Beta <= Alpha, pruning");
+                    // println!("Beta <= Alpha, pruning");
                     break;
                 }
             }
