c9d24bdcc24843edf6e90b3c6fb7c95ced8b20ce
DoormatIka <trashcanartistlily@gmail.com>
2024-02-14 01:14:12 +0800

    moved go responsiblity to process thread

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate vampirc_uci;
 use peak_alloc::PeakAlloc;
 use std::fmt::Display;
 use std::time::Duration;
-use vampirc_uci::{parse, UciMove, UciMessage, UciTimeControl, UciSearchControl};
+use vampirc_uci::{parse, UciMove, UciMessage, UciTimeControl, UciSearchControl, UciSquare, UciPiece};
 
 use std::sync::mpsc::{self, Sender, Receiver, TryRecvError};
 use std::io::stdin;
@@ -21,30 +21,14 @@ pub mod game;
 #[global_allocator]
 static PEAK_ALLOC: PeakAlloc = PeakAlloc;
 
-/**
- * For the write thread to get.
- */
-enum Command {
-    // name
-    UciOk(String),
-}
-
-impl Display for Command {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Command::UciOk(name) => write!(f, "{}", name),
-        }
-        
-    }
-}
-
 fn main() {
     let starting = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let almost_mate = "6k1/1p3ppp/8/8/8/3q1bP1/5K1P/8 b - - 0 1    ";
 
-    let (output_tx, output_rx): (Sender<Command>, Receiver<Command>) = mpsc::channel();
+    let (output_tx, output_rx): (Sender<UciMessage>, Receiver<UciMessage>) = mpsc::channel();
     let (input_tx, input_rx): (Sender<UciMessage>, Receiver<UciMessage>) = mpsc::channel();
 
+    // TODO: not really sure what isready's behavior is..
     let is_ready = Arc::new(RwLock::new(true));
     let out_ready = Arc::clone(&is_ready);
 
@@ -68,10 +52,55 @@ fn main() {
                 Ok(out) => {
                     *out_ready.write().unwrap() = false;
                     match out {
-                        Command::UciOk(ready) => {
-                            thread::sleep(Duration::from_secs(2));
+                        UciMessage::UciOk => {
+                            println!("id name Cirno");
+                            println!("id author twoleaflotus");
                             println!("uciok");
                         },
+
+                        // engine responsibilities, so "go" has to be here
+                        UciMessage::Go { time_control, search_control } => {
+                            if let Some(time_control) = time_control {
+                                match time_control {
+                                    // the act of thinking during the opponent's turn.
+                                    // we have separate threads so this should be easy to implement.
+                                    //      however, i don't really see a gain with 
+                                    //      pondering (with the techniques rn), so no thanks.
+                                    // 
+                                    // (opinion)
+                                    // also pondering can only be worth it *if* 
+                                    //      the engine & player is on the same level.
+                                    //      since pondering searches through the tree and does a null-move (?)
+                                    //      and it only becomes worth if the player plays the expected move.
+                                    // https://www.chessprogramming.org/Pondering
+                                    UciTimeControl::Ponder => (),
+
+                                    // Search until the "stop" command.
+                                    // Cannot be implemented at the moment.
+                                    UciTimeControl::Infinite => (),
+
+                                    // Search exactly for x milliseconds.
+                                    // Cannot be implemented at the moment.
+                                    UciTimeControl::MoveTime(_time) => (),
+
+                                    // Notifies the engine of how much time there is left.
+                                    UciTimeControl::TimeLeft { .. } => (),
+                                };
+                            };
+                            if let Some(search_control) = search_control {
+                                search_control.depth; // the only supported option we have.
+                                
+                                let best_move = UciMessage::best_move(UciMove {
+                                    from: UciSquare { file: 'e', rank: 5 }, 
+                                    to: UciSquare { file: 'e', rank: 7 }, 
+                                    promotion: None,
+                                });
+
+                                thread::sleep(Duration::from_secs(3));
+                                println!("{}", best_move);
+                            };
+                        }
+                        _ => {},
                     }
                     *out_ready.write().unwrap() = true;
                 },
@@ -86,13 +115,13 @@ fn main() {
     });
 
 
-    loop {
-        let input = input_rx.recv().expect("Failed to recieve from input thread.");
+    loop { // this part might seem useless but its not.
+        let uci_message = input_rx.recv().expect("Failed to recieve from input thread.");
 
-        match input {
+        match uci_message {
             // switches the executable from not UCI to UCI-mode
             UciMessage::Uci => {
-                output_tx.send(Command::UciOk(String::from("Cirno")))
+                output_tx.send(UciMessage::UciOk)
             },
 
             // can be used by the GUI to check if the engine is ready or online
@@ -100,8 +129,6 @@ fn main() {
             UciMessage::IsReady => {
                 if *is_ready.read().unwrap() {
                     println!("{}", UciMessage::ReadyOk);
-                } else {
-                    println!("readynotok (this is not standard UCI)");
                 }
                 Ok(())
             },
@@ -113,48 +140,17 @@ fn main() {
             }
             
             // sets up the board with a fen string and some moves
-            //
             // btw, this is where "position startpos moves" will go to
-            UciMessage::Position { startpos, fen, moves } => { Ok(()) },
-            
+            UciMessage::Position { .. } => { 
+                output_tx.send(uci_message)
+            },
             // allows the engine to start calculating on current position
-            UciMessage::Go { time_control, search_control } => {
-                if let Some(time_control) = time_control {
-                    match time_control {
-                        // the act of thinking during the opponent's turn.
-                        // we have separate threads so this should be easy to implement.
-                        //      however, i don't really see a gain with 
-                        //      pondering (with the techniques rn), so no thanks.
-                        // 
-                        // (opinion)
-                        // also pondering can only be worth it *if* 
-                        //      the engine & player is on the same level.
-                        //      since pondering searches through the tree and does a null-move (?)
-                        //      and it only becomes worth if the player plays the expected move.
-                        // https://www.chessprogramming.org/Pondering
-                        UciTimeControl::Ponder => (),
-
-                        // Search until the "stop" command.
-                        // Cannot be implemented at the moment.
-                        UciTimeControl::Infinite => (),
-
-                        // Search exactly for x milliseconds.
-                        // Cannot be implemented at the moment.
-                        UciTimeControl::MoveTime(_time) => (),
-
-                        // Notifies the engine of how much time there is left.
-                        UciTimeControl::TimeLeft { .. } => (),
-                    };
-                };
-                if let Some(search_control) = search_control {
-                    search_control.depth; // the only supported option we have.
-                };
-                Ok(()) 
+            UciMessage::Go { .. } => {
+                output_tx.send(uci_message)
             }
             // stops all calculations.
             UciMessage::Stop => { Ok(()) }
             _ => {
-                println!("Invalid command.");
                 Ok(())
             },
         }.expect("Main thread can't send to output/process thread");
